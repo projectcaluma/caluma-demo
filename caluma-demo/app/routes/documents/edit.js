899756cd0b6d30bfe96acf7bf51418a1ff83be6b
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2019-09-12 12:51:36 +0200

    feat(documents): cancel the creation of a document

--- caluma-demo/app/routes/documents/edit.js
+++ caluma-demo/app/routes/documents/edit.js
@@ -1,7 +1,29 @@
 import Route from "@ember/routing/route";
+import { queryManager } from "ember-apollo-client";
+import gql from "graphql-tag";
+import { action } from "@ember/object";
 
 export default class DocumentsEditRoute extends Route {
+  @queryManager apollo;
+
   model(params) {
     return params.uuid;
+  }
+
+  @action
+  async deleteDocument(document) {
+    await this.apollo.mutate({
+      mutation: gql`
+        mutation($document: ID!) {
+          removeDocument(input: { document: $document }) {
+            document {
+              id
+            }
+          }
+        }
+      `,
+      variables: { document }
+    });
+    this.transitionTo("documents");
   }
 }
