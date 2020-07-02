80aba54d80b2a34b0244127db016c3d69a15af54
Lukas Grossar <lukas.grossar@adfinis.com>
2020-07-02 11:18:11 +0200

    Move proxy_pass to caluma to a variable
    When running within Kubernetes this avoid a startup error, because the
    hostname can't be resolved and within Kubernetes we're implementing the
    functionality of the proxy_pass in Ingress.

--- caluma-demo/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-rm /etc/nginx/conf.d/default.conf.template
-
-exec "$@"
